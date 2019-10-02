f5846f3b893637cdf809f035264039940206fcb9
Dianna Tingg <dtingg@gmail.com>
2019-10-02 15:06:43 -0700

    added edit and update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   end
   
   def show
-    book_id = params[:id].to_i
+    # book_id = params[:id].to_i
+    book_id = params[:id]
     @book = Book.find_by(id: book_id)
     
     if @book.nil?
@@ -12,4 +13,40 @@ class BooksController < ApplicationController
       return
     end
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(author: params[:book][:author], title: params[:book][:title], description: params[:book][:description]) #instantiate a new book
+    if @book.save # save returns true if the database insert succeeds
+      redirect_to books_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+      render :new # show the new book form view again
+      return
+    end
+  end
+  
+  def edit
+    book_id = params[:id]
+    @book = Book.find_by(id: book_id)
+    
+    if @book.nil?
+      head :not_found
+      return
+    end    
+  end
+  
+  def update
+    book_id = params[:id]
+    @book = Book.find_by(id: book_id)
+    
+    @book.update(author: params[:book][:author], title: params[:book][:title], description: params[:book][:description])
+    redirect_to books_path 
+    return
+    
+  end
+  
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -3,14 +3,14 @@ require "test_helper"
 describe BooksController do
   describe "Index method" do
     it "can render the index" do
-      get "/books"
+      get '/books'
       must_respond_with :success
     end
   end
   
   describe "Show method" do
     it "can show the page for a single book" do
-      get "/books/1"
+      get '/books/1'
       must_respond_with :success
     end
     
